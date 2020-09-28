f18190fa74b49ce8e079b3e25b67c3b994cc5c4d
Shasel <github email address>
2020-09-28 01:51:21 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Maru", 3 , "male", "brown", "fiesty", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Bob", 4 , "male", "white", "call", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Joe", 3 , "male", "brown", "fiesty", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Carl", 4 , "male", "black", "calm", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Paul", 5 , "male", "brown", "fiesty", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Stacy", 10 , "female", "brown", "fiesty", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Leslie", 2 , "female", "brown", "calm", "alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3 , "male", "brown", "fiesty", "alive");
